68919e9a8565f8271a6a53dd3eac193451d7b9ac
vinit giri <vinitgiri2007@gmail.com>
2026-01-03 17:29:38 +0530

    Initial portfolio commit

---
+++ src/lib/content/experience.ts
@@ -0,0 +1,21 @@
+import { ExperienceSectionType } from '@/lib/types/sections';
+
+export const experienceSection: ExperienceSectionType = {
+  title: "where i've worked",
+  experiences: [
+    {
+      company: 'Greater Noida Institute of Technology',
+      companyUrl: 'https://www.gniotgroup.edu.in/',
+      role: 'BTech Student',
+      started: 'June 2024',
+      upto: 'Present',
+      tasks: [
+        'Pursuing Bachelor of Technology with a focus on building strong fundamentals in computer science.',
+        'Actively learning and applying web development concepts through academic and personal projects.',
+        'Working with modern technologies such as HTML, CSS, JavaScript, React, and Next.js.',
+        'Improving problem-solving skills by practicing Data Structures and Algorithms.',
+        'Collaborating with peers on college projects and participating in technical activities.'
+      ],
+    },
+  ],
+};
---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,39 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/vinitgiriii',
+  instagram: 'https://www.instagram.com/vinitgiriii',
+  twitter: 'https://x.com/vinitgiri37',
+  github: 'https://github.com/Vinitgiri37',
+  linkedin: 'https://www.linkedin.com/in/vinitgiri2007/',
+};
+
+export const author = {
+  name: 'Vinit Giri',
+  email: 'vinitgiri2007@gmail.com',
+};
+
+export const seoData = {
+  title: 'Vinit Giri | Front-End Developer',
+  description:
+    'Vinit giri is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://vatsalsinghkv.vercel.app/',
+  keywords: [
+    'Vinit',
+    'Vinit giri',
+    '@vinitgiri',
+    'vinitgiri',
+    'Portfolio',
+    'vinit Portfolio ',
+    'Vinit Giri Portfolio',
+  ],
+};
