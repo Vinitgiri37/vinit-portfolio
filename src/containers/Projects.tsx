9f1a07af01e8bcb29913d3d38417aac1d3015988
vinit giri <vinitgiri2007@gmail.com>
2026-02-23 20:38:52 +0530

    Fix projects module properly

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,10 +1,10 @@
 'use client';
+
 import { projectsSection } from '@/lib/content/projects';
 import { PROJECTS_INITIALLY } from '@/lib/utils/config';
 import { sortByYear } from '@/lib/utils/helper';
 
 import { Button, ProjectCard, Wrapper } from '@/components';
-
 import { getSectionAnimation, projectVariants } from '@/styles/animations';
 
 import { motion } from 'framer-motion';
@@ -13,8 +13,8 @@ import { useState } from 'react';
 const Projects = () => {
   const { projects, title } = projectsSection;
   const [showMore, setShowMore] = useState(false);
-  const topProjects = projects.slice(0, PROJECTS_INITIALLY);
 
+  const topProjects = projects.slice(0, PROJECTS_INITIALLY);
   const visibleProjects = showMore ? projects : topProjects;
 
   return (
@@ -22,34 +22,21 @@ const Projects = () => {
       <motion.h2 className="heading-secondary text-center !mb-12">
         {title}
       </motion.h2>
-      <div className="grid gap-6 grid-cols-auto-250 xs:grid-cols-auto-300 place-items-center">
-        {sortByYear(visibleProjects).map((project, i) => {
-          if (i < PROJECTS_INITIALLY) {
-            return (
-              <ProjectCard
-                {...project}
-                key={project.id}
-                variants={projectVariants}
-                initial="hidden"
-                whileInView="show"
-                custom={i}
-                viewport={{ once: true }}
-              />
-            );
-          }
 
-          return (
-            <ProjectCard
-              {...project}
-              key={project.id}
-              variants={projectVariants}
-              initial="hidden"
-              animate="show"
-              custom={i - PROJECTS_INITIALLY}
-            />
-          );
-        })}
+      <div className="grid gap-6 grid-cols-auto-250 xs:grid-cols-auto-300 place-items-center">
+        {sortByYear(visibleProjects).map((project, i) => (
+          <ProjectCard
+            {...project}
+            key={project.id}
+            variants={projectVariants}
+            initial="hidden"
+            whileInView="show"
+            custom={i}
+            viewport={{ once: true }}
+          />
+        ))}
       </div>
+
       {projects.length > PROJECTS_INITIALLY && (
         <Button
           size="lg"
